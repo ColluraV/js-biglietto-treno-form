97d02afb00dfc39a8702a32bda59ed88413ab295
ColluraV <valerio.18.02.89@hotmail.it>
2023-07-03 16:15:29 +0200

    copiato l'esercizio precedente da modificare

---
+++ js/main.js
@@ -0,0 +1,84 @@
+
+
+//20% sconto minorenni
+//40% sconto senior over 65 
+//0.21€ al km
+
+// dati utente 
+
+const firstName = prompt(" inserisci il tuo nome ");
+const secondName = prompt(" inserisci il tuo cognome ");
+const età = prompt(" inserisci la tua età ");
+const distanza = prompt(" inserisci la distanza della tratta espressa in Km");
+
+const tariffa = 0.21;
+
+
+const date = new Date();
+const currentDay = date.getDate();
+const currentMonth = date.getUTCMonth() + 1;
+const currentYear = date.getFullYear();
+const currentDate = ` ${currentDay}/${currentMonth}/${currentYear} `;
+
+
+//numero biglietto
+const primoMin =  0;
+const primoMax = 15;
+const primoNum = Math.round(Math.random() * (primoMax - primoMin + 1)) + primoMin;
+
+
+const secondoMin = 10;
+const secondoMax = 99;
+const secondoNum = Math.round(Math.random() * (secondoMax - secondoMin + 1)) + secondoMin;
+
+const terzoMin = 10;
+const terzoMax = 99;
+const terzoNum = Math.round(Math.random() * (terzoMax - terzoMin + 1)) + terzoMin;
+
+const numBiglietto = ( "#" + primoNum + secondoNum + terzoNum );
+
+console.log(numBiglietto);
+
+const prezzoLordo = ( distanza * tariffa );
+const ridottoMinori = ( prezzoLordo - (prezzoLordo * 0.20 )  )
+const ridottoSenior = ( prezzoLordo - ( prezzoLordo * 0.40 ) )
+
+console.log( prezzoLordo.toFixed(2) );
+console.log( ridottoMinori.toFixed(2) );
+console.log( ridottoSenior.toFixed(2) );
+
+console.log(`Ridotto minori ` + ridottoMinori.toFixed(2) );
+console.log( `Ridotto Senior `+ ridottoSenior.toFixed(2) );
+
+
+
+if( età < 18 ){
+    document.getElementById("prezzo").innerHTML = (ridottoMinori.toFixed(2)) ;
+    document.getElementById("fasciaPrezzo").innerHTML = (`Ridotto minori`) ;
+
+
+}else if(età < 65) {
+    document.getElementById("prezzo").innerHTML = ( prezzoLordo.toFixed(2)) ;
+    document.getElementById("fasciaPrezzo").innerHTML = (`Prezzo Pieno` ) ;
+
+
+}else if(età > 65) {
+        document.getElementById("prezzo").innerHTML = ( ridottoSenior.toFixed(2)) ;
+        document.getElementById("fasciaPrezzo").innerHTML = ( `Ridotto Senior`) ;
+
+    }
+
+
+
+
+document.getElementById("firstName").innerHTML = ` ${firstName}`;
+document.getElementById("secondName").innerHTML = ` ${secondName}`;
+document.getElementById("distanza").innerHTML = `Distanza ${distanza} Km`;
+document.getElementById("data").innerHTML = `${currentDate}`;
+document.getElementById("numBiglietto").innerHTML = `${numBiglietto}`;
+
+
+
+
+
+
